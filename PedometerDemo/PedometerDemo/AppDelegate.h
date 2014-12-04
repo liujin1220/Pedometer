7eb22a43e273ad74a21d91be218f0a7c1515d4e0
刘锦 <522978593@qq.com>
2014-12-04 19:58:32 +0800

    添加框架

---
+++ PedometerDemo/PedometerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PedometerDemo
+//
+//  Created by 刘锦 on 14/12/4.
+//  Copyright (c) 2014年 liujin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PedometerDemo/PedometerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PedometerDemo
+//
+//  Created by 刘锦 on 14/12/4.
+//  Copyright (c) 2014年 liujin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
